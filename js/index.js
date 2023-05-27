d76ff77e5ea8c94c78ebe2a9b56fcf23f8f375d8
radmanplays <95340057+radmanplays@users.noreply.github.com>
2023-05-27 13:18:05 +0330

    Update index.js

--- js/index.js
+++ js/index.js
@@ -47,7 +47,7 @@ async function isBlocked(url) {
   try {
     var README = await fetch(url + '/README.md');
     var content = await README.text();
-    if (content.startsWith('# 3kh0 Assets')) {
+    if (content.startsWith('# 3kh0X Assets')) {
       // The CDN is not blocked
       return false;
     } else {
